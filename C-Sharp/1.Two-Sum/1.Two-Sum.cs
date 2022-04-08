81dade226bec98e6b9598fdb6f43da7e03bd73b5
Alberto Kato <albertokatojr@gmail.com>
2022-04-08 20:43:57 -0300

    [C#] 1 - 1. Two Sum
    [C#] 1 - 1. Two Sum

---
+++ C-Sharp/1.Two-Sum/1.Two-Sum.cs
@@ -0,0 +1,22 @@
+﻿namespace C_Sharp.TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (i == j) continue;
+
+                    if ((nums[i] + nums[j]) == target)
+                    {
+                        return new int[] {i,j};
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+}
