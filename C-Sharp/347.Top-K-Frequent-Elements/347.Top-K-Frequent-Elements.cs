261de2ed19f3a7c59f03ab2467c39041501987cd
Alberto Kato <albertokatojr@gmail.com>
2022-04-08 22:17:17 -0300

    [C#] 347. Top K Frequent Elements

---
+++ C-Sharp/347.Top-K-Frequent-Elements/347.Top-K-Frequent-Elements.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace C_Sharp.TopKFrequentElements
+{
+    public class Solution
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> numsTopFrequent = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(!numsTopFrequent.ContainsKey(nums[i]))
+                {
+                    numsTopFrequent.Add(nums[i],1);
+                } 
+                else
+                {
+                    ++numsTopFrequent[nums[i]];
+                }
+                
+            }
+
+            return numsTopFrequent.OrderByDescending(n => n.Value).Take(k).Select(k => k.Key).ToArray();
+        }
+    }
+}
