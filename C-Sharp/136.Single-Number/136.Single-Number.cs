191cf9894b137f5d3dbb87e46f75c838ec08b390
Alberto Kato <albertokatojr@gmail.com>
2022-04-09 15:27:53 -0300

    [C#] 136. Single Number

---
+++ C-Sharp/136.Single-Number/136.Single-Number.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace C_Sharp.SingleNumber
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var x = nums
+                .GroupBy(x => x)
+                .Where(x => x.Count() == 1)
+                .Select(x => x.Key)
+                .ToArray();
+           return x[0];  
+        }
+    }
+}
