47b796dc91fdd564e9ff1b9e62b03f3f7dba015e
Alberto Kato <albertokatojr@gmail.com>
2022-04-08 21:08:51 -0300

    [C#] 9 - Palindrome Number

---
+++ C-Sharp/9.Palindrome-Number/9.Palindrome-Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace C_Sharp.PalindromeNumber
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string reversed = new string(x.ToString().Reverse().ToArray());
+            if (x < 0) return false;
+            return x.ToString() == reversed;   
+        }
+    }
+}
