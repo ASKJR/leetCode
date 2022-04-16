7b5706357f15bd9796b75f46452c9fd81cd3d3a7
Alberto Kato <albertokatojr@gmail.com>
2022-04-15 22:24:14 -0300

    [C#] - 1528. Shuffle String

---
+++ C-Sharp/1528.Shuffle-String/1528.Shuffle-String.cs
@@ -0,0 +1,17 @@
+﻿namespace C_Sharp.ShuffleString
+{
+    public class Solution
+    {
+        public string RestoreString(string s, int[] indices)
+        {
+            char[] chars = new char[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                chars[indices[i]] = s[i];
+            }
+
+            return new string(chars);
+        }
+    }
+}
