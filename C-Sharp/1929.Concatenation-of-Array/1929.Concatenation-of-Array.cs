16d6b1fe7ab55cfbfa2b41fc8a9839564217cfa1
Alberto Kato <albertokatojr@gmail.com>
2022-04-15 22:59:40 -0300

    [C#] - 1929. Concatenation of Array

---
+++ C-Sharp/1929.Concatenation-of-Array/1929.Concatenation-of-Array.cs
@@ -0,0 +1,21 @@
+﻿namespace C_Sharp.ConcatenationOfArray
+{
+    public class Solution
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            int[] answer = new int[nums.Length*2];
+            int index = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    answer[index] = nums[j];
+                    ++index;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
