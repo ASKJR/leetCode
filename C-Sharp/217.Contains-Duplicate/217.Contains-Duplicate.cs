9fa8d89c0d50f2820a35aaf1a8df34a38ee24d2f
Alberto Kato <albertokatojr@gmail.com>
2022-04-15 22:31:54 -0300

    [C#] - 217. Contains Duplicate

---
+++ C-Sharp/217.Contains-Duplicate/217.Contains-Duplicate.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace C_Sharp.ContainsDuplicate
+{
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            return nums.Distinct().Count() != nums.Length;
+        }
+    }
+}
